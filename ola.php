f40a94b779ce4c88660077f061ac18adc93d4c8e
Nelson Caixeirinho <108085055+NelsonCaixeirinho@users.noreply.github.com>
2022-06-26 22:21:38 +0100

    Create ola.php

---
+++ ola.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-pt">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Primeiro exemplo PHP</title>
+</head>
+<body>
+    <h1>Testando PHP</h1>
+    <?php
+        echo "Olá, Mundo!";
+    ?>
+    
+</body>
+</html>
